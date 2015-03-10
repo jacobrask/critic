93dd6d77aec07c67519ebd7a64407db349272ee6
Jacob Rask <jacob@jacobrask.net>
2015-03-10 22:25:23 +0100

    fixup! Concatenate and copy frontend resources
    Add note about CORS workaround

--- web/development.js
+++ web/development.js
@@ -8,4 +8,6 @@ Config.DEBUG = true;
 
 // URL to a valid Critic REST API endpoint.
 // If it's not a relative URL, the server has to give CORS access.
+// As a temporary work-around for CORS Chromium-based browsers, give the flag
+// --disable-web-security when starting your browser.
 Config.API_ROOT = "https://critic-review.org/api/v1/";
